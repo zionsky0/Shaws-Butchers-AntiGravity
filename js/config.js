63f60f9b35db3f54b29b19be91b0e164e6403bd3
DaddyZION <kingzionsky@gmail.com>
2026-06-28 18:49:12 +0100

    ss

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ===== GLOBAL CONFIGURATION =====
 // Stores the Google Sheets Web App URL and WhatsApp number for synchronized ordering.
 // Hardcode your values here to share them globally with all customers and devices.
-const DEFAULT_GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbwvjda-m8iuUPg_Y39LRpbuAOVDprZhL4pnqLf-sXQtsDVuDWe9geHb56oAisFV3c8pMg/exec";
+const DEFAULT_GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbxAnO4WXLMHAPO6wd3lbi3NOD9WA_ym8BfRUeFXI4VX5RBI3J7r4atIqQ7xyN2p43oEIg/exec";
 const DEFAULT_WHATSAPP_NUMBER = "07361808431";
 const DEFAULT_RECAPTCHA_SITE_KEY = "YOUR_RECAPTCHA_SITE_KEY_HERE";
 
