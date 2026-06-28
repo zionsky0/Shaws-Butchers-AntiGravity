de63fd227c4a8ea4832e9d6266974ec27a22060d
DaddyZION <kingzionsky@gmail.com>
2026-06-28 19:30:44 +0100

    please

--- js/config.js
+++ js/config.js
@@ -5,8 +5,14 @@ const DEFAULT_GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbwbfkf
 const DEFAULT_WHATSAPP_NUMBER = "07361808431";
 const DEFAULT_RECAPTCHA_SITE_KEY = "6LeCpjotAAAAAGUKeMrrGxMQhKQmw0F7rqGTUU4n";
 
+const cleanStorageValue = (key, defaultVal) => {
+  const val = localStorage.getItem(key);
+  if (!val || val.includes("YOUR_")) return defaultVal || "";
+  return val;
+};
+
 window.GLOBAL_CONFIG = {
-  googleSheetUrl: localStorage.getItem('shaws_google_sheet_url') || DEFAULT_GOOGLE_SHEET_URL || "",
-  whatsappNumber: localStorage.getItem('shaws_whatsapp_number') || DEFAULT_WHATSAPP_NUMBER || "",
-  recaptchaSiteKey: localStorage.getItem('shaws_recaptcha_site_key') || DEFAULT_RECAPTCHA_SITE_KEY || ""
+  googleSheetUrl: cleanStorageValue('shaws_google_sheet_url', DEFAULT_GOOGLE_SHEET_URL),
+  whatsappNumber: cleanStorageValue('shaws_whatsapp_number', DEFAULT_WHATSAPP_NUMBER),
+  recaptchaSiteKey: cleanStorageValue('shaws_recaptcha_site_key', DEFAULT_RECAPTCHA_SITE_KEY)
 };
